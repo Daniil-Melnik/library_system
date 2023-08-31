e23f1bcbf58a510d49ac6976c37346f81b30fb09
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-08-31 23:16:51 +0300

    database for library is working

--- init_db.py
+++ init_db.py
@@ -18,13 +18,13 @@ cur.execute('DROP TABLE IF EXISTS dmel_book_authors;')
 cur.execute('DROP TABLE IF EXISTS dmel_authors;')
 
 cur.execute('CREATE TABLE dmel_books (id serial PRIMARY KEY,'
-                                  'title varchar (150) NOT NULL,'
-                                  'discription varchar (1500) NOT NULL,'
-                                  'file bytea DEFAULT NULL);'          
-                                 )
+                                 'title varchar (150) NOT NULL,'
+                                 'discription varchar (500) NOT NULL,'
+                                 'file bytea DEFAULT NULL);'          
+                                  )
 
 cur.execute('CREATE TABLE dmel_tags (id serial PRIMARY KEY,'
-                                  'tag varchar (150) NOT NULL);'
+                                  'tagg varchar (150) NOT NULL);'
                                   )
 
 cur.execute('CREATE TABLE dmel_authors (id serial PRIMARY KEY,'
@@ -41,15 +41,30 @@ cur.execute('CREATE TABLE dmel_book_tags (id serial PRIMARY KEY,'
                                   'book_id integer NOT NULL);'
                                   )
 
+
 print ("made")
 
 # Insert data into the table
 
-# cur.execute('INSERT INTO dmel_books (title, discription, file)'
-#             'VALUES (?, ?, NULL)',
-#             ('C++++',
-#              'Charles Dickens')
-#             )
+cur.execute('INSERT INTO dmel_books (title, discription, file)'
+            'VALUES (%s, %s, NULL)',
+            ('C++++',
+             'Charles Dickens')
+            )
+
+cur.execute('INSERT INTO dmel_book_tags (tag_id, book_id)'
+            'VALUES (%s, %s)',
+            (25, 14)
+            )
+
+cur.execute('INSERT INTO dmel_book_authors (author_id, book_id)'
+            'VALUES (%s, %s)',
+            (5, 4)
+            )
+
+cur.execute("INSERT INTO dmel_tags VALUES (%s, %s);", (5, "qooq") )
+
+cur.execute("INSERT INTO dmel_authors VALUES (%s, %s);", (1, "Tolstoy") )
 
 conn.commit()
 
