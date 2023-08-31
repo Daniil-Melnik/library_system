4d2f94b46ef60d722ffbbe3d8b2c8a090d75c4ee
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-08-31 22:25:31 +0300

    added initialisation of database for library

--- init_db.py
+++ init_db.py
@@ -11,34 +11,45 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 # Execute a command: this creates a new table
-cur.execute('DROP TABLE IF EXISTS books;')
-cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
-                                 'title varchar (150) NOT NULL,'
-                                 'author varchar (50) NOT NULL,'
-                                 'pages_num integer NOT NULL,'
-                                 'review text,'
-                                 'date_added date DEFAULT CURRENT_TIMESTAMP);'
+cur.execute('DROP TABLE IF EXISTS dmel_books;')
+cur.execute('DROP TABLE IF EXISTS dmel_book_tags;')
+cur.execute('DROP TABLE IF EXISTS dmel_tags;')
+cur.execute('DROP TABLE IF EXISTS dmel_book_authors;')
+cur.execute('DROP TABLE IF EXISTS dmel_authors;')
+
+cur.execute('CREATE TABLE dmel_books (id serial PRIMARY KEY,'
+                                  'title varchar (150) NOT NULL,'
+                                  'discription varchar (1500) NOT NULL,'
+                                  'file bytea DEFAULT NULL);'          
                                  )
+
+cur.execute('CREATE TABLE dmel_tags (id serial PRIMARY KEY,'
+                                  'tag varchar (150) NOT NULL);'
+                                  )
+
+cur.execute('CREATE TABLE dmel_authors (id serial PRIMARY KEY,'
+                                  'author varchar (150) NOT NULL);'
+                                  )
+
+cur.execute('CREATE TABLE dmel_book_authors (id serial PRIMARY KEY,'
+                                  'author_id integer NOT NULL,'
+                                  'book_id integer NOT NULL);'
+                                  )
+
+cur.execute('CREATE TABLE dmel_book_tags (id serial PRIMARY KEY,'
+                                  'tag_id integer NOT NULL,'
+                                  'book_id integer NOT NULL);'
+                                  )
+
 print ("made")
 
 # Insert data into the table
 
-cur.execute('INSERT INTO books (title, author, pages_num, review)'
-            'VALUES (%s, %s, %s, %s)',
-            ('A Tale of Two Cities',
-             'Charles Dickens',
-             489,
-             'A great classic!')
-            )
-
-
-cur.execute('INSERT INTO books (title, author, pages_num, review)'
-            'VALUES (%s, %s, %s, %s)',
-            ('Anna Karenina',
-             'Leo Tolstoy',
-             864,
-             'Another great classic!')
-            )
+# cur.execute('INSERT INTO dmel_books (title, discription, file)'
+#             'VALUES (?, ?, NULL)',
+#             ('C++++',
+#              'Charles Dickens')
+#             )
 
 conn.commit()
 
