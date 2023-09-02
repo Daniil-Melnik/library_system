478d434c8bfddc266bd35abaeb9afc022e0a4834
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-09-03 02:57:34 +0300

    all attributes in tables

--- init_db.py
+++ init_db.py
@@ -4,9 +4,11 @@ import psycopg2
 
 conn = psycopg2.connect(
         host="localhost",
-        database="flask_db",
+        database="library_system",
         user="user_1",
         password="1234")
+
+print (conn)
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
@@ -20,6 +22,8 @@ cur.execute('DROP TABLE IF EXISTS dmel_authors;')
 
 cur.execute('CREATE TABLE dmel_books (id serial PRIMARY KEY,'
                                  'title varchar (150) NOT NULL,'
+                                 'year integer NOT NULL,'
+                                 'num_pg integer NOT NULL,'
                                  'discription varchar (500) NOT NULL,'
                                  'file bytea DEFAULT NULL);'          
                                   )
@@ -42,20 +46,25 @@ cur.execute('CREATE TABLE dmel_book_tags (id serial PRIMARY KEY,'
                                   'book_id integer NOT NULL);'
                                   )
 
+cur.execute('CREATE TABLE test (id serial PRIMARY KEY,'
+                                  'tag_id integer NOT NULL,'
+                                  'book_id integer NOT NULL);'
+                                  )
+
 
 print ("made")
 
 # Insert data into the table
 
-path="./Melnik_1308.pdf"
-f = open(path,'rb')
-dat = f.read()
-binary = psycopg2.Binary(dat)
+# path="./Melnik_1308.pdf"
+# f = open(path,'rb')
+# dat = f.read()
+# binary = psycopg2.Binary(dat)
 
-cur.execute('INSERT INTO dmel_books (title, discription, file)'
-            'VALUES (%s, %s, %s)',
-            ('C++++', 'Charles Dickens', binary)
-            )
+# cur.execute('INSERT INTO dmel_books (title, discription, file)'
+#             'VALUES (%s, %s, NULL)',
+#             ('Q++++', 'Charles Dickens')
+#             )
 
 # cur.execute('INSERT INTO dmel_book_tags (tag_id, book_id)'
 #             'VALUES (%s, %s)',
@@ -73,18 +82,18 @@ cur.execute('INSERT INTO dmel_books (title, discription, file)'
 
 conn.commit()
 
-cur.execute('SELECT * from dmel_books')
+# cur.execute('SELECT * from dmel_books')
 
-book = cur.fetchone()
-print(book[3])
+# book = cur.fetchone()
+# print(book[1])
 
-f = open('code.txt', "wb")
-f.write(book[3].tobytes())
+# f = open('code.txt', "wb")
+# f.write(book[3].tobytes())
 
-file = open('new.pdf', 'wb')
-for line in open('code.txt', 'rb').readlines():
-    file.write(line)
-file.close()
+# file = open('new.pdf', 'wb')
+# for line in open('code.txt', 'rb').readlines():
+#     file.write(line)
+# file.close()
 
 # print(book[3].tobytes())
 
